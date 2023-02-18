01cb8b1973ff29c3c6bbb18dab613bd64e72874d
Lean Mijares <99194388+leanmyself@users.noreply.github.com>
2023-02-19 03:49:27 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,56 +19,71 @@ window.addEventListener("scroll", about);
 
 
 $(function(){
-
     window.sr = ScrollReveal();
     
     if ($(window).width() < 768) {
-    
       if ($('.timeline-content').hasClass('js--fadeInLeft')) {
         $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
       }
     
       sr.reveal('.js--fadeInRight', {
         origin: 'right',
-        distance: '300px',
+        distance: '200px',
         easing: 'ease-in-out',
-        duration: 800,
+        duration: 500,
       });
     
     } else {
       
       sr.reveal('.js--fadeInLeft', {
         origin: 'left',
-        distance: '300px',
+        distance: '200px',
         easing: 'ease-in-out',
-        duration: 800,
+        duration: 500,
       });
     
       sr.reveal('.js--fadeInRight', {
         origin: 'right',
-        distance: '300px',
+        distance: '200px',
         easing: 'ease-in-out',
-        duration: 800,
+        duration: 500,
       });
     
     }
     
     sr.reveal('.js--fadeInLeft', {
         origin: 'left',
-        distance: '300px',
+        distance: '200px',
         easing: 'ease-in-out',
-        duration: 800,
+        duration: 500,
       });
     
       sr.reveal('.js--fadeInRight', {
         origin: 'right',
-        distance: '300px',
+        distance: '200px',
         easing: 'ease-in-out',
-        duration: 800,
+        duration: 500,
       });
-    
-    
 });  
+
+
+  $(document).on('scroll', function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  $(document).on('click', 'a.scroll-to-top', function(e) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top)
+    }, 1000, 'easeInOutExpo');
+    e.preventDefault();
+  });
+
 
 $(document).ready(function() {
       $("#project-slider").owlCarousel({
